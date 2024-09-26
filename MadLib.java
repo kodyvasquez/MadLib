9fad4bf04822567dd46e65a43473c90ac6dbe2e1
Kody Vasquez <kody.vasquez@student.tamdistrict.org>
2024-09-26 09:59:02 -0700

    add first input from reader

--- MadLib.java
+++ MadLib.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class MadLib {
     public static void main(String[] args)
     {
+       String madlib = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
+    //    madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+    //    madlib = ".......";
+       Scanner scanner = new Scanner(System.in);
+
+       int openingBracketIndex = madlib.indexOf("<");
+       int closingBracketIndex = madlib.indexOf(">");
+
+       String madlibFirstWord = madlib.substring(openingBracketIndex +1, closingBracketIndex);
+       System.out.println("Enter a " + madlibFirstWord);
+
+       String userInput = scanner.nextLine();
+
+
+
+
+
+       
 
 
 
