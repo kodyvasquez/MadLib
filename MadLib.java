31adc995f0983c8f64fb2533778956711ac89de5
Kody Vasquez <kody.vasquez@student.tamdistrict.org>
2024-11-03 20:06:59 -0800

    third word completed (project complete)

--- MadLib.java
+++ MadLib.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class MadLib {
    public static void main(String[] args) {
-      String madlib = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
-      // madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
-      // madlib = ".......";
+      // String madlib = "I went to the animal <noun>, the <plural noun> and the
+      // <number> beasts were there.";
+      // String madlib = "Once upon a time in a <adjective> land, a <noun> <adverb>
+      // grew.";
+      String madlib = "The <adjective> comet flashed <adverb> across the <noun>";
       Scanner scanner = new Scanner(System.in);
 
       // Find where the first unknown is
@@ -27,9 +29,21 @@ public class MadLib {
       System.out.println("Enter a " + madlibSecondWord + ": ");
       String userInput2 = scanner.nextLine();
       // Put that word back into the madlib
-      firstPart = madlib.substring(0, openingBracketIndex);
-      secondPart = madlib.substring(closingBracketIndex + 1);
-      madlib = firstPart + userInput + secondPart;
+      firstPart = madlib.substring(0, openingBracketIndex2);
+      secondPart = madlib.substring(closingBracketIndex2 + 1);
+      madlib = firstPart + userInput2 + secondPart;
+
+      // Find where the third unknown is
+      int openingBracketIndex3 = madlib.indexOf("<");
+      int closingBracketIndex3 = madlib.indexOf(">");
+      String madlibThirdWord = madlib.substring(openingBracketIndex3 + 1, closingBracketIndex3);
+      // Prompt reader to put in third word
+      System.out.println("Enter a " + madlibThirdWord + ": ");
+      String userInput3 = scanner.nextLine();
+      // Put that word back into the madlib
+      firstPart = madlib.substring(0, openingBracketIndex3);
+      secondPart = madlib.substring(closingBracketIndex3 + 1);
+      madlib = firstPart + userInput3 + secondPart;
 
       System.out.println(madlib);
 
